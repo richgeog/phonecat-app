6fe5fd176f34b3f750266e8d0143ead0a309cf60
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-10 23:42:02 +0000

    added a test for service factory

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,5 +3,9 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
+  beforeEach(module('phonecatApp'));
 
+  it('check the avalability of Phone Factory', inject(function(Phone) {
+    expect(Phone).toBeDefined();
+  }));
 });
